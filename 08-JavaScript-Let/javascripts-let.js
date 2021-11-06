9774df666d1e387296de34f41789d6233a36e0e1
= <=>
2021-11-06 22:04:35 +0600

    added javascripts operators

--- 08-JavaScript-Let/javascripts-let.js
+++ 08-JavaScript-Let/javascripts-let.js
@@ -1,4 +1,4 @@
 
 let x = 5;
-let y = "Jibon"; // SyntaxError: Identifier 'x' has already been declared
+let x = "Jibon"; // SyntaxError: Identifier 'x' has already been declared
 console.log(x)
---
+++ 10-Javascripts-Operators/javascripts-Operators.js
@@ -0,0 +1,43 @@
+
+
+// Adding Strings and Numbers
+
+var i = 5;
+var j = "7";
+var resutl = i + j;
+console.log("Strings and Numbers : ", resutl);
+// NB : Adding two numbers, will return the sum, but adding a number and a string will return a string:
+console.log("Type : ", typeof(resutl));
+console.log("\n");
+
+
+// JavaScript Comparison Operators
+/*
+    Operator	Description
+        ==	    equal to
+        ===	    equal value and equal type
+        !=	    not equal
+        !==	    not equal value or not equal type
+        >	    greater than
+        <	    less than
+        >=	    greater than or equal to
+        <=	    less than or equal to
+        ?	    ternary operator
+*/
+
+// Example
+var a = 7;
+var b = 8;
+var result =  a == b;
+console.log("Comparison Operators : ", resutl);
+
+// Example of ternary operator
+// condition ? expression_1 : expression_2;
+
+var age = 19;
+var canDrive = age > 16 ? 'yes' : 'no';
+console.log("ternary operator :", canDrive)
+
+
+// JavaScript Bitwise Operators
+
