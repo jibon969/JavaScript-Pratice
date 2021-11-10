7ff38a8f6aa11168ad1fa703b94dbddfbdfd0e75
= <=>
2021-11-10 23:52:31 +0600

    added js events

---
+++ 16-JavaScript-Events/javaScriptEvents.js
@@ -0,0 +1,14 @@
+// javaScript Events
+
+/*
+
+HTML Events
+
+    An HTML event can be something the browser does, or something a user does.
+    Here are some examples of HTML events:
+    
+    An HTML web page has finished loading
+    An HTML input field was changed
+    An HTML button was clicked
+
+*/
